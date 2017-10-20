cabcee3c9616d33d23a4975ed3e9208aab531143
Fabrício Matté <ultcombo@gmail.com>
2017-10-20 00:52:08 -0200

    Use `decorateBrowserOptions` API to set fullscreen

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-exports.onWindow = (win) => {
-  win.once('show', () => {
-    setTimeout(() => {
-      win.setFullScreen(true);
-    }, 50);
-  });
-};
+'use strict';
+
+exports.decorateBrowserOptions = defaults => Object.assign({}, defaults, {
+  fullscreen: true
+});
+
+// Expose a no-op API method to workaround Hyper issue:
+// https://github.com/zeit/hyper/issues/2377
+exports.decorateConfig = config => config;
